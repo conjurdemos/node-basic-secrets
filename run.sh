ffdfee70f5ba683cefa582042a3f881857d2d17b
Kevin Gilpin <kgilpin@gmail.com>
2015-10-19 16:37:50 -0400

    Run script

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+host="host/host-01.node-basic-secrets.demo"
+read -s -p "Enter API key for $host: " api_key
+
+env CONJUR_AUTHN_LOGIN=$host CONJUR_AUTHN_API_KEY=$api_key conjur env run -- node app.js
